7eff24a906b642c25dd8aaf11faa058f0cf5c8c2
sanopopular <prabeshbhandari842@gmail.com>
2023-09-19 17:33:45 +0545

    login page form validation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
-const validateForm = ()=> {
-    let name = document.getElementById('username').value
-    let password = document.getElementById('password').value
-    let errorMessage = ''
+const form = document.querySelector("form"),
+emailField = form.querySelector(".email-field"),
+emailInput = form.querySelector(".email"),
+passwordField = form.querySelector(".password-field"),
+passwordInput = form.querySelector(".password")
 
-    //email validation
-    if(name == ''){
-        errorMessage = 'usename cannot be left empty'
+//emailValidation
+const checkEmail = () => {
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(!emailInput.value.match(emailPattern)){
+        return emailField.classList.toggle("invalid")
     }
 }
+
+//password validation
+const checkPassword = (e) => {
+    const loginPassword = "password"
+    if(passwordInput.value == '' || passwordInput.value == null || passwordInput.value !== loginPassword){
+        return passwordField.classList.toggle("invalid");
+    }
+}
+
+
+//calling the functions on form submission
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); //preventing form submission
+    checkEmail();
+    checkPassword();
+    emailInput.addEventListener("keydown", checkEmail);
+    passwordInput.addEventListener("click", checkPassword);
+});
